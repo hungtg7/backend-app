1d96f107c98d85c5c9191e24c3cd1af78c831ea6
hungtg7 <hung.trangia@customore.co>
2023-03-13 00:39:45 +0700

    test: concurrency

--- app/celery.py
+++ app/celery.py
@@ -1,14 +1,14 @@
 from celery import Celery
 
 app = Celery('app',
-             broker='amqp://',
-             backend='rpc://',
+             broker='amqp://localhost:5672',
+             backend='redis://localhost:6379/0',
              include=['app.task'])
 
 # Optional configuration, see the application user guide.
 app.conf.update(
     result_expires=3600,
 )
-
+app.conf.task_protocol = 1
 if __name__ == '__main__':
     app.start()
--- app/task.py
+++ app/task.py
@@ -1,16 +1,30 @@
 from .celery import app
+import time
+from celery import group, chain as celery_chain, shared_task
 
-
-@app.task
+@app.task()
 def add(x, y):
+    print('proces')
+    time.sleep(10)
     return x + y
 
 
-@app.task
+@app.task()
 def mul(x, y):
+    print("mul")
     return x * y
 
+@app.task()
+def deliver(x):
+    print('reciever')
+    sig = group([add.s(i, i+1).set(queue='test') | mul.s(2).set(queue='test') for i in range(0, x)])
+    t = sig.apply_async()
+    return t
 
-@app.task
-def xsum(numbers):
-    return sum(numbers)
+@app.task()
+def xsum(x):
+    sig = group([add.s(i, i+1) | mul.s(2) | deliver.s().set(queue='test') for i in range(0, x)])
+    t = sig.apply_async()
+    return t
+
+    # return sum(numbers)
