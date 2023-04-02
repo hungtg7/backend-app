b32829ec78fc2da666295dd746ac5dd0900aca03
hungtg7 <hungtran511997@gmail.com>
2023-04-03 00:45:30 +0700

    chore: rewmove  unused import

--- app/authen/service/login.go
+++ app/authen/service/login.go
@@ -4,11 +4,9 @@ import (
 	"context"
 	"crypto/rand"
 	"encoding/base64"
-	"fmt"
 	"net/http"
 	"time"
 )
-
 
 func (s *Service) OauthGoogleLogin(ctx context.Context) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -16,14 +14,13 @@ func (s *Service) OauthGoogleLogin(ctx context.Context) func(http.ResponseWriter
 		oauthState := generateStateOauthCookie(w)
 
 		/*
-		AuthCodeURL receive state that is a token to protect the user from CSRF attacks. You must always provide a non-empty string and
-		validate that it matches the the state query parameter on your redirect callback.
+			AuthCodeURL receive state that is a token to protect the user from CSRF attacks. You must always provide a non-empty string and
+			validate that it matches the the state query parameter on your redirect callback.
 		*/
 		u := s.googleOauthConfig.AuthCodeURL(oauthState)
 		http.Redirect(w, r, u, http.StatusTemporaryRedirect)
 	}
 }
-
 
 func generateStateOauthCookie(w http.ResponseWriter) string {
 	var expiration = time.Now().Add(20 * time.Minute)
